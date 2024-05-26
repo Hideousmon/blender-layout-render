01d61f5a266e0d36512bfda57484da295f1a6c21
Hideousmon <mailtozyzhao@163.com>
2024-05-26 12:50:32 +0800

    add setup.py

--- bpycanvas/__init__.py
+++ bpycanvas/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.0.1"
+
 from .waveguide import Waveguide
 from .camera import add_camera, add_workbench_camera
 from .arrow import Arrow
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("bpycanvas/__init__.py") as fin:
+    for line in fin:
+        if line.startswith("__version__ ="):
+            version = eval(line[14:])
+            break
+
+setup(name='bpycanvas',
+      version=version,
+      description='A Blender modeling tool with a similar modeling method to SPLayout.',
+      author='Zhenyu ZHAO',
+      author_email='mailtozyzhao@163.com',
+      install_requires=[],
+      # long_description=long_description,
+      url="https://github.com/Hideousmon/blender-layout-render",
+      packages=find_packages()
+      )
