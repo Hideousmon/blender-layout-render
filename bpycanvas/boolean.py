d95e9542c73c74c533d64ec17b2900cfc411cac9
Hideousmon <mailtozyzhao@163.com>
2024-10-07 19:59:25 +0800

    add solver option for cut op

--- bpycanvas/boolean.py
+++ bpycanvas/boolean.py
@@ -1,9 +1,10 @@
 import bpy
 
-def cut(obj_1, obj_2):
+def cut(obj_1, obj_2, solver="EXACT"):
     bool_modifier = obj_1.modifiers.new(name="Boolean_Cut", type='BOOLEAN')
     bool_modifier.object = obj_2
     bool_modifier.operation = 'DIFFERENCE'
+    bool_modifier.solver = solver
 
     bpy.context.view_layer.objects.active = obj_1
     bpy.ops.object.modifier_apply(modifier=bool_modifier.name)
